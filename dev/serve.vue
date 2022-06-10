77268427c1ec9ad0c234bb0f927223c35405c222
Gabriel Corvalan <gabrielcorvalan@MacBook-Pro-de-Gabriel.local>
2022-06-09 21:07:19 -0300

    create library

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { DevSubtreeLibrarySample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  DevSubtreeLibrarySample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <dev-subtree-library-sample />
+  </div>
+</template>
